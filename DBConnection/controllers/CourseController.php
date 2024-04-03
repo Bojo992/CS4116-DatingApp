b83b0ed18f23952c23279d4db3e5d34b392ba93f
bojo992 <93447305+zed992@users.noreply.github.com>
2024-04-03 15:04:31 +0100

    Refactored folders

--- DBConnection/controllers/CourseController.php
+++ DBConnection/controllers/CourseController.php
@@ -4,7 +4,7 @@ header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: X-Requested-With');
 header('Content-Type: application/json');
 
-include("DatabaseCourse.php");
+include("./db/DatabaseCourse.php");
 
 class CourseController {
     private $course;
