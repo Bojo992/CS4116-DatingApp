47214050d1440a72ef47c3c9976eb011fb307286
Craig_ <21340633@studentmail.ul.ie>
2024-04-05 11:46:29 +0100

    [Craig] Moved navbar to index.html w/ css to make it global

--- Angular/src/app/app.routes.ts
+++ Angular/src/app/app.routes.ts
@@ -2,7 +2,8 @@ import {Routes} from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {HomepageComponent} from "./homepage/homepage.component";
 
-export const routes = [
-  {path: 'login', component: LoginComponent},
-  {path: 'home', component: HomepageComponent}
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomepageComponent },
 ];
