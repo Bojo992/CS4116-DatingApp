cebf870f3d579a668092efe468d411ce012dd616
MartyD1 <coolkidmarty@gmail.com>
2024-04-11 15:14:52 +0100

    Profile page component

--- Angular/src/app/app.routes.ts
+++ Angular/src/app/app.routes.ts
@@ -7,11 +7,13 @@ import {LoginComponent} from "./login/login.component";
 import {HomepageComponent} from "./homepage/homepage.component";
 import { RegistrationquizComponent } from './registrationquiz/registrationquiz.component';
 import { AdminpageComponent } from './adminpage/adminpage.component';
+import { MyprofileComponent } from './myprofile/myprofile.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomepageComponent, canActivate: [AuthGuard] },
   { path: 'register', component: RegistrationquizComponent},
-  { path: 'admin', component: AdminpageComponent}
+  { path: 'admin', component: AdminpageComponent},
+  { path: 'profile', component: MyprofileComponent}
 ];
