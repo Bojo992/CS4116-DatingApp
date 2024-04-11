1006934e2f3c04e47097f787600a199d322cd7b3
MartyD1 <coolkidmarty@gmail.com>
2024-04-11 17:21:16 +0100

    Placeholder profile page

--- Angular/src/app/profile/profile.component.ts
+++ Angular/src/app/profile/profile.component.ts
@@ -3,6 +3,7 @@ import { CookieService } from 'ngx-cookie-service';
 import { UserService } from '../DBConnection/user.service';
 import { ActivatedRoute } from '@angular/router';
 import { switchMap } from 'rxjs'; // This is used to switch the route when a new parameter is passed
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 
 @Component({
@@ -17,19 +18,28 @@ import { switchMap } from 'rxjs'; // This is used to switch the route when a new
 export class ProfileComponent implements OnInit {
   userId: string;
 
-  constructor(private cookieService: CookieService, private route: ActivatedRoute) {
+  constructor(private cookieService: CookieService, private route: ActivatedRoute, private userService: UserService, private snackBar: MatSnackBar) {
     this.userId = '';
   }
 
   ngOnInit(): void {
     // Subscribe to the route parameters
     this.route.paramMap.subscribe(params => {
-      this.userId = params.get('id') || this.cookieService.get('UserId');
+      this.userId = params.get('id') || this.cookieService.get('UID');
       
-      if(this.userId === this.cookieService.get('UserId')){
+      if(this.userId === this.cookieService.get('UID')){
         console.log("Viewing YOUR profile");
+        this.snackBar.open('Viewing your profile', 'Close',{
+          duration: 2000,
+          verticalPosition: 'bottom'
+        })
+    
       } else {
         console.log("Viewing someone else's profile with id: " + this.userId);
+        this.snackBar.open('Viewing profile with userid: ', this.userId,{
+          duration: 2000,
+          verticalPosition: 'bottom'
+        })
       }
  
     });
