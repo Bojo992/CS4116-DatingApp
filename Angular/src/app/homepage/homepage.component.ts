d5580e376abbc943f9d2bf34d1345ad7ef31d675
Craig_ <21340633@studentmail.ul.ie>
2024-04-16 17:37:15 +0100

    [Craig] Hardcoded like/dislike system for homepage

--- Angular/src/app/homepage/homepage.component.ts
+++ Angular/src/app/homepage/homepage.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {Component, Input, OnInit} from '@angular/core';
+import {FormControl, FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {
   MatAutocomplete,
   MatAutocompleteModule,
@@ -7,15 +7,21 @@ import {
   MatOption
 } from "@angular/material/autocomplete";
 import {AsyncPipe, NgForOf, NgIf} from "@angular/common";
+import {ProfileComponent} from "../profile/profile.component";
+import {map, Observable, startWith, Subscription} from "rxjs";
+import {UserService} from "../DBConnection/user.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {CookieService} from "ngx-cookie-service";
+import {Router} from "@angular/router";
 
 interface User {
-  userId: number;
-  personalInfo: number;
-  course: number;
-  dateCreated: string;
-  isAdmin: number;
-  userName: string; 
-  userEmail: string;
+  userId: number,
+  personalinfo: number,
+  course: number,
+  isAdmin: boolean,
+  userName: string,
+  userEmail: string,
+  dateCreated: string
 }
 
 @Component({
@@ -31,9 +37,49 @@ interface User {
     AsyncPipe,
     NgForOf,
     NgIf,
+    ProfileComponent,
   ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
-export class HomepageComponent{
+export class HomepageComponent implements OnInit {
+  index = 0;
+  users: User[] = [];
+  @Input()
+  test!: User | null;
+  userSubscription: Subscription | undefined;
+
+  constructor(private userService: UserService, private snackBar: MatSnackBar, private cookieService: CookieService, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    // Fetch all users
+    this.userService.getAll().subscribe((users: any) => {
+      for (let i of users) {
+        this.users.push(i);
+      }
+    });
+  }
+
+  clickLike(): void {
+    // Move to the next profile
+    if (this.index < this.users.length - 1) {
+      this.index++;
+      const nextUserId = this.users[this.index].userId;
+      // Call the loadProfileData method of ProfileComponent
+      const profileComponent = document.querySelector('app-profile') as any;
+      if (profileComponent) {
+        profileComponent.loadProfileData(nextUserId);
+      }
+    } else {
+      this.snackBar.open('No more profiles to load','Close',{
+        duration: 2000,
+        verticalPosition: 'bottom'
+      })
+      console.log('No more profiles to load');
+    }
+  }
+
+  protected readonly ProfileComponent = ProfileComponent;
 }
+
