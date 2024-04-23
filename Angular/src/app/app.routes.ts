83e799efa8b193e907a5e1d39cea684689235848
Boris Bobylkov <93447305+Bojo992@users.noreply.github.com>
2024-04-23 14:43:12 +0100

    Update app.routes.ts

--- Angular/src/app/app.routes.ts
+++ Angular/src/app/app.routes.ts
@@ -13,8 +13,7 @@ import { RegisterStepperComponent } from './register-stepper/register-stepper.co
 import {SearchPageComponent} from "./search-page/search-page.component";
 
 export const routes: Routes = [
-  // { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '', redirectTo: 'search-page', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomepageComponent, canActivate: [AuthGuard] },
   { path: 'register', component: RegistrationquizComponent},
