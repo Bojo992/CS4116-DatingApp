dfb1f18dd71222ffdb3ef70f833164df5aae14db
MartyD1 <coolkidmarty@gmail.com>
2024-04-23 12:06:40 +0100

    Fixed some funky stuff here, profiles reload when searched

--- Angular/src/app/profile/profile.component.ts
+++ Angular/src/app/profile/profile.component.ts
@@ -52,94 +52,94 @@ export class ProfileComponent implements OnInit {
   dateCreated: string = '';
   isLoading: boolean = false;
   myprofile: boolean = false;
-
+  
   public userIdFromHomePage : number = 0;
-
+  
   @Input() userProfile: User | null = null;
   personalInfo: PersonalInfo | null = null;
   university: University | null = null;
   course: Course | null = null;
   userCourse: UserCourse | null = null;
   Gender : string = '';
-
+  
   constructor(private cookieService: CookieService, private route: ActivatedRoute, private userService: UserService, private snackBar: MatSnackBar, private profileService: ProfileService, private router : Router) {
     this.userId = '';
     this.username = '';
   }
-
+  
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      this.userId = (this.userProfile != null) ? this.userProfile.userId.toString() : (params.get('id') || this.cookieService.get('UID'));
-
+    this.route.paramMap.subscribe(params => { 
+      
+      const routeUserId = params.get('id');       // Getting the user id from the route parameter
+      this.userId = routeUserId ? routeUserId : this.cookieService.get('UID'); // fallback to cookie if no route parameter
+      
       this.isLoading = true;
-
-      if (this.userId === this.cookieService.get('UID')) {
+      this.myprofile = this.userId === this.cookieService.get('UID');
+      
+      if (this.myprofile) {
         console.log("Viewing YOUR profile");
-        this.myprofile = true;
-        this.snackBar.open('Viewing your profile', 'Close', {
+        this.snackBar.open('Viewing your profile', 'Close', { 
           duration: 2000,
-          verticalPosition: 'bottom'
-        });
-      } else {
-        console.log("Viewing someone else's profile with id: " + this.userId);
-        this.myprofile = false;
-        this.snackBar.open('Viewing profile with userid: ', this.userId, {
-          duration: 2000,
-          verticalPosition: 'bottom'
+          verticalPosition:
+          'bottom' });
+        } else {
+          this.snackBar.open(`Viewing profile with userid: ${this.userId}`, 'Close', {
+            duration: 2000,
+            verticalPosition:
+            'bottom' });
+          }
+          
+          this.loadProfileData(this.userId);
         });
       }
-
-      this.loadProfileData(this.userId);
-      console.log('loading data for userid ' + this.userId)
-    });
-  }
-
-  loadProfileData(userId: string): void {
-    console.log('loading data for userid ' + this.userId)
-    this.profileService.getProfileInfo(userId).subscribe({
-      next: (response: any) => {
-        if (response.user && response.user.length) {
-          this.userProfile = response.user[0];
-          console.log('loading data for userid ' + this.userId)
-          this.personalInfo = response.personalInfo[0];
-          console.log(this.personalInfo);
-          this.university = response.university[0];
-          this.course = response.course[0];
-          this.userCourse = response.userCourse[0];
-          this.isLoading = false;
+      
+      loadProfileData(userId: string): void {
+        console.log('loading data for userid ' + this.userId)
+        this.profileService.getProfileInfo(userId).subscribe({
+          next: (response: any) => {
+            if (response.user && response.user.length) {
+              this.userProfile = response.user[0];
+              console.log('loading data for userid ' + this.userId)
+              this.personalInfo = response.personalInfo[0];
+              console.log(this.personalInfo);
+              this.university = response.university[0];
+              this.course = response.course[0];
+              this.userCourse = response.userCourse[0];
+              this.isLoading = false;
+            }
+          },
+          error: (error) => {
+            this.snackBar.open('Failed to load data', 'Close', {
+              duration: 3000,
+            });
+            console.error('There was an error!', error);
+          }
+        });
+      }
+      editBio() {
+        this.snackBar.open('Edit Bio Placeholder', 'Close', {
+          duration: 2000,
+          verticalPosition: 'bottom'
+        });
+        
+      }
+      
+      getLifestyleIcons(): string {
+        let icons = '';
+        
+        if (this.personalInfo?.smoking === 1) {
+          icons += '<i class="fas fa-smoking"></i> ';
         }
-      },
-      error: (error) => {
-        this.snackBar.open('Failed to load data', 'Close', {
-          duration: 3000,
-        });
-        console.error('There was an error!', error);
+        if (this.personalInfo?.drinking === 1) {
+          icons += '<i class="fas fa-beer"></i> ';
+        }
+        if (this.personalInfo?.vegan === 1) {
+          icons += '<i class="fa-solid fa-leaf"></i> ';
+        }
+        return icons;
       }
-    });
-  }
-  editBio() {
-    this.snackBar.open('Edit Bio Placeholder', 'Close', {
-      duration: 2000,
-      verticalPosition: 'bottom'
-    });
-
-  }
-
-  getLifestyleIcons(): string {
-    let icons = '';
-
-    if (this.personalInfo?.smoking === 1) {
-      icons += '<i class="fas fa-smoking"></i> ';
+      
+      
+      
     }
-    if (this.personalInfo?.drinking === 1) {
-      icons += '<i class="fas fa-beer"></i> ';
-    }
-    if (this.personalInfo?.vegan === 1) {
-      icons += '<i class="fa-solid fa-leaf"></i> ';
-    }
-    return icons;
-  }
-
-
-
-}
+    
