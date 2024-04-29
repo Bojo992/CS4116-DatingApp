f58e5b69cc79581822533bb02656ee917eb30d3c
Craig_ <21340633@studentmail.ul.ie>
2024-04-29 18:42:12 +0100

    [Craig] Matching api connections setup for search and homepage matching.

--- Angular/src/app/homepage/homepage.component.ts
+++ Angular/src/app/homepage/homepage.component.ts
@@ -13,6 +13,7 @@ import {ProfileService} from "../DBConnection/profile.service";
 import {MatSnackBar} from "@angular/material/snack-bar";
 import {CookieService} from "ngx-cookie-service";
 import {Router} from "@angular/router";
+import {MatchingService} from "../DBConnection/matching.service";
 
 interface User {
   userId: number,
@@ -49,7 +50,7 @@ export class HomepageComponent implements OnInit {
   @Input()
   test!: User | null;
 
-  constructor(private profileService: ProfileService, private snackBar: MatSnackBar, private cookieService: CookieService, private router: Router) {
+  constructor(private matchingService: MatchingService ,private profileService: ProfileService, private snackBar: MatSnackBar, private cookieService: CookieService, private router: Router) {
   }
 
   ngOnInit(): void {
@@ -59,8 +60,8 @@ export class HomepageComponent implements OnInit {
     let userId = +this.cookieService.get('UID')
     this.profileService.getSuggestionForUser(userId).subscribe((users: any) => {
       for (let i of users) {
-          this.users.push(i);
-          console.log(i);
+        this.users.push(i);
+        console.log(i);
       }
     });
 
@@ -81,6 +82,72 @@ export class HomepageComponent implements OnInit {
   }
 
   clickLike(): void {
+    this.matchingService.like(this.cookieService.get('UID'), this.users[this.index].userId).subscribe((result: any) => {
+      console.log("like response", result);
+
+      if (result) {
+        this.snackBar.open('Like successful', 'Close', {
+          duration: 3000,
+          verticalPosition: 'bottom'
+        })
+        console.log("Like was successful");
+      } else {
+        this.snackBar.open('Match was found', 'Close', {
+          duration: 3000,
+          verticalPosition: 'bottom'
+        })
+        console.log("Like was not successful");
+      }
+    });
+
+    if (this.users.length === 0) {
+      console.log('No profile found');
+      this.snackBar.open('No more profiles to load', 'Close', {
+        duration: 2000,
+        verticalPosition: 'bottom'
+      })
+    } else {
+      // Move to the next profile
+      if (this.index < this.users.length - 1) {
+
+        this.index++;
+        const nextUserId = this.users[this.index].userId;
+        // Call the loadProfileData method of ProfileComponent
+        const profileComponent = document.querySelector('app-profile') as any;
+        if (profileComponent) {
+          profileComponent.loadProfileData(nextUserId);
+          /*this.shuffleUsers();
+          this.users.splice(this.index, 1);*/
+        }
+      } else {
+        this.snackBar.open('No more profiles to load', 'Close', {
+          duration: 2000,
+          verticalPosition: 'bottom'
+        })
+        console.log('No more profiles to load');
+      }
+    }
+  }
+
+  clickDislike(): void {
+    this.matchingService.dislike(this.cookieService.get('UID'), this.users[this.index].userId).subscribe((result : any) => {
+      console.log(result);
+
+      if (result) {
+        this.snackBar.open('Dislike successful','Close',{
+          duration: 3000,
+          verticalPosition: 'bottom'
+        })
+        console.log("Dislike was successful");
+      } else {
+        this.snackBar.open('Dislike unsuccessful','Close',{
+          duration: 3000,
+          verticalPosition: 'bottom'
+        })
+        console.log("Dislike was not successful");
+      }
+    });
+
     if (this.users.length === 0) {
       console.log('No profile found');
       this.snackBar.open('No more profiles to load', 'Close', {
